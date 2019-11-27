a3a7f01fcf87f5092b09677efab4cb97210629c1
Marcos Rocha <marcosviniciusrochadasilva@gmail.com>
2019-11-27 16:22:48 -0300

    Adicao de Telas pelo Macbook

---
+++ app/src/main/java/br/edu/farol/gadoplus/ui/pesagem/PesagemAddEditActivity.java
@@ -0,0 +1,14 @@
+package br.edu.farol.gadoplus.ui.pesagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PesagemAddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesagem_add_edit);
+    }
+}
